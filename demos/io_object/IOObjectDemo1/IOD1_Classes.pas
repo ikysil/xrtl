1e316d81f0f070628ef912d214292e83867153a9
Illya Kysil <ikysil@gmail.com>
2004-03-16 15:27:20 +0000

    git-svn-id: svn://svn.berlios.de/xrtl/XRTL/trunk@24 8151b8e4-99fc-cf4b-a0c8-eaa015f54a2e

--- demos/io_object/IOObjectDemo1/IOD1_Classes.pas
+++ demos/io_object/IOObjectDemo1/IOD1_Classes.pas
@@ -2,6 +2,232 @@ unit IOD1_Classes;
 
 interface
 
+uses
+  Windows,
+  SysUtils,
+  xrtl_util_Value,
+  xrtl_reflect_ClassDescriptor, xrtl_reflect_Introspector,
+  xrtl_reflect_PropertyList, xrtl_reflect_Property,
+  xrtl_io_object_Serializer;
+
+type
+  TBase = class
+  private
+    FName: string;
+  public
+    constructor Create;
+    property   Name: string read FName;
+  end;
+
+  TBaseClassDescriptor = class(TXRTLClassDescriptor)
+  protected
+    procedure  DoDefineProperties(const Properties: IXRTLPropertyList); override;
+    procedure  DoGetValues(const Obj: TObject; const Properties: IXRTLPropertyList); override;
+    procedure  DoSetValues(const Obj: TObject; const Properties: IXRTLPropertyList); override;
+    function   DoCreateInstance: TObject; override;
+  public
+    constructor Create;
+  end;
+
+  TBaseStreamer = class(TXRTLObjectStreamer)
+    procedure  WriteObjectData(const Writer: IXRTLObjectWriter; const Obj: TObject); override;
+    procedure  ReadNoData(const Obj: TObject); override;
+    procedure  ReadObjectData(const Reader: IXRTLObjectReader; const Obj: TObject); override;
+  end;
+
+  TDerived1 = class(TBase)
+  private
+    FParent: TBase;
+  public
+    constructor Create(const AParent: TBase);
+    property   Parent: TBase read FParent;
+  end;
+
+  TDerived1ClassDescriptor = class(TXRTLClassDescriptor)
+  protected
+    procedure  DoDefineProperties(const Properties: IXRTLPropertyList); override;
+    procedure  DoGetValues(const Obj: TObject; const Properties: IXRTLPropertyList); override;
+    procedure  DoSetValues(const Obj: TObject; const Properties: IXRTLPropertyList); override;
+    function   DoCreateInstance: TObject; override;
+  public
+    constructor Create;
+  end;
+
+  TDerived1Introspector = class(TXRTLIntrospector)
+    procedure  DefineProperties(const Descriptor: IXRTLClassDescriptor;
+                                const Properties: IXRTLPropertyList); override;
+    procedure  GetValues(const Obj: TObject; const Properties: IXRTLPropertyList); override;
+    procedure  SetValues(const Obj: TObject; const Properties: IXRTLPropertyList); override;
+  end;
+
+  TDerived2 = class(TDerived1)
+  public
+    constructor Create;
+  end;
+
+  TDerived2ClassDescriptor = class(TXRTLClassDescriptor)
+  protected
+    procedure  DoDefineProperties(const Properties: IXRTLPropertyList); override;
+    procedure  DoGetValues(const Obj: TObject; const Properties: IXRTLPropertyList); override;
+    procedure  DoSetValues(const Obj: TObject; const Properties: IXRTLPropertyList); override;
+    function   DoCreateInstance: TObject; override;
+  public
+    constructor Create;
+  end;
+
 implementation
+
+{ TBase }
+
+constructor TBase.Create;
+begin
+  inherited Create;
+  FName:= IntToStr(GetTickCount);
+end;
+
+{ TBaseClassDescriptor }
+
+constructor TBaseClassDescriptor.Create;
+begin
+  inherited Create('iodemo::TBase', TBase);
+end;
+
+function TBaseClassDescriptor.DoCreateInstance: TObject;
+begin
+  Result:= TBase.Create;
+end;
+
+procedure TBaseClassDescriptor.DoDefineProperties(const Properties: IXRTLPropertyList);
+begin
+end;
+
+procedure TBaseClassDescriptor.DoGetValues(const Obj: TObject; const Properties: IXRTLPropertyList);
+begin
+end;
+
+procedure TBaseClassDescriptor.DoSetValues(const Obj: TObject; const Properties: IXRTLPropertyList);
+begin
+end;
+
+{ TBaseStreamer }
+
+procedure TBaseStreamer.WriteObjectData(const Writer: IXRTLObjectWriter; const Obj: TObject);
+begin
+  Writer.WriteUTF8String((Obj as TBase).FName);
+end;
+
+procedure TBaseStreamer.ReadNoData(const Obj: TObject);
+begin
+end;
+
+procedure TBaseStreamer.ReadObjectData(const Reader: IXRTLObjectReader; const Obj: TObject);
+begin
+  (Obj as TBase).FName:= Reader.ReadUTF8String;
+end;
+
+{ TDerived1 }
+
+constructor TDerived1.Create(const AParent: TBase);
+begin
+  inherited Create;
+  FParent:= AParent;
+end;
+
+{ TDerived1ClassDescriptor }
+
+constructor TDerived1ClassDescriptor.Create;
+begin
+  inherited Create('iodemo::TDerived1', TDerived1, nil, TDerived1Introspector.Create);
+end;
+
+function TDerived1ClassDescriptor.DoCreateInstance: TObject;
+begin
+  Result:= TDerived1.Create(nil);
+end;
+
+procedure TDerived1ClassDescriptor.DoDefineProperties(const Properties: IXRTLPropertyList);
+begin
+end;
+
+procedure TDerived1ClassDescriptor.DoGetValues(const Obj: TObject; const Properties: IXRTLPropertyList);
+begin
+end;
+
+procedure TDerived1ClassDescriptor.DoSetValues(const Obj: TObject; const Properties: IXRTLPropertyList);
+begin
+end;
+
+{ TDerived1Introspector }
+
+procedure TDerived1Introspector.DefineProperties(const Descriptor: IXRTLClassDescriptor;
+  const Properties: IXRTLPropertyList);
+begin
+  Properties.Add(TXRTLProperty.Create('Parent', XRTLValue(TObject(nil))));
+end;
+
+procedure TDerived1Introspector.GetValues(const Obj: TObject; const Properties: IXRTLPropertyList);
+var
+  LObj: TDerived1;
+begin
+  LObj:= Obj as TDerived1;
+  XRTLSetValue(Properties.GetByName('Parent').Value, LObj.FParent);
+end;
+
+procedure TDerived1Introspector.SetValues(const Obj: TObject; const Properties: IXRTLPropertyList);
+var
+  LObj: TDerived1;
+begin
+  LObj:= Obj as TDerived1;
+  LObj.FParent:= XRTLGetAsObject(Properties.GetByName('Parent').Value) as TBase;
+end;
+
+{ TDerived2 }
+
+constructor TDerived2.Create;
+begin
+  inherited Create(nil);
+end;
+
+{ TDerived2ClassDescriptor }
+
+constructor TDerived2ClassDescriptor.Create;
+begin
+  inherited Create('iodemo::TDerived2', TDerived2);
+end;
+
+function TDerived2ClassDescriptor.DoCreateInstance: TObject;
+begin
+  Result:= TDerived2.Create;
+end;
+
+procedure TDerived2ClassDescriptor.DoDefineProperties(const Properties: IXRTLPropertyList);
+begin
+end;
+
+procedure TDerived2ClassDescriptor.DoGetValues(const Obj: TObject; const Properties: IXRTLPropertyList);
+begin
+end;
+
+procedure TDerived2ClassDescriptor.DoSetValues(const Obj: TObject; const Properties: IXRTLPropertyList);
+begin
+end;
+
+procedure RegisterClasses;
+var
+  Descriptor: IXRTLClassDescriptor;
+begin
+  Descriptor:= TBaseClassDescriptor.Create;
+  XRTLRegisterClassDescriptor(Descriptor);
+  XRTLRegisterStreamer(TBaseStreamer.Create(Descriptor));
+  Descriptor:= TDerived1ClassDescriptor.Create;
+  XRTLRegisterClassDescriptor(Descriptor);
+  Descriptor:= TDerived2ClassDescriptor.Create;
+  XRTLRegisterClassDescriptor(Descriptor);
+end;
+
+initialization
+begin
+  RegisterClasses;
+end;
 
 end.
--- demos/io_object/IOObjectDemo1/IOObjectDemo1.dpr
+++ demos/io_object/IOObjectDemo1/IOObjectDemo1.dpr
@@ -4,7 +4,68 @@ program IOObjectDemo1;
 
 uses
   SysUtils,
-  IOD1_Classes in 'IOD1_Classes.pas';
+  IOD1_Classes in 'IOD1_Classes.pas',
+  xrtl_io_Stream, xrtl_io_FileStream,
+  xrtl_io_object_Serializer, xrtl_io_object_BinarySerializer,
+  xrtl_util_Value;
+
+procedure Demo;
+var
+  O1, I1: TBase;
+  O2, I2: TDerived1;
+  O3, I3: TDerived2;
+  LOS: TXRTLOutputStream;
+  LIS: TXRTLInputStream;
+  LSer: IXRTLSerializer;
+  LDeser: IXRTLDeserializer;
+begin
+  O1:= nil;
+  O2:= nil;
+  O3:= nil;
+  I1:= nil;
+  I2:= nil;
+  I3:= nil;
+  LOS:= nil;
+  LSer:= nil;
+  LDeser:= nil;
+  try
+    O1:= TBase.Create;
+    O2:= TDerived1.Create(O1);
+    O3:= TDerived2.Create;
+    LOS:= TXRTLFileOutputStream.Create('c:\test.ser', False);
+    LSer:= TXRTLBinarySerializer.Create;
+    LSer.WriteObject(LOS, nil);
+//    LSer.WriteObject(LOS, O2);
+    LSer.WriteObject(LOS, O1);
+    LSer.WriteObject(LOS, O3);
+    LOS.Close;
+    LIS:= TXRTLFileInputStream.Create('c:\test.ser', False);
+    LDeser:= TXRTLBinaryDeserializer.Create;
+    I2:= LDeser.ReadObject(LIS, I2) as TDerived1;
+    I1:= LDeser.ReadObject(LIS, I1) as TBase;
+    I3:= LDeser.ReadObject(LIS, I3) as TDerived2;
+  finally
+    LDeser:= nil;
+    LSer:= nil;
+    FreeAndNil(LIS);
+    FreeAndNil(LOS);
+    FreeAndNil(I3);
+    FreeAndNil(I2);
+    FreeAndNil(I1);
+    FreeAndNil(O3);
+    FreeAndNil(O2);
+    FreeAndNil(O1);
+  end;
+end;
 
 begin
+  try
+    Demo;
+  except
+    on E: Exception do
+    begin
+      WriteLn(Format('Exception: %s', [E.Message]));
+      ReadLn;
+    end;
+  end;
 end.
